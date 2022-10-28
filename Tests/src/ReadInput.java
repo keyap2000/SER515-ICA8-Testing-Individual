137dfe3522fb3a918511a62c9dc2a4fc2e079757
keyap2000 <112652577+keyap2000@users.noreply.github.com>
2022-10-27 21:43:59 -0700

    file doesn't exist test case

--- Tests/src/ReadInput.java
+++ Tests/src/ReadInput.java
@@ -2,24 +2,30 @@ import java.io.*;
 import java.util.Scanner;
 
 public class ReadInput {
+    String output = "";
 
-    public ReadInput() throws FileNotFoundException{
+    public ReadInput() throws FileNotFoundException {
 
-        File file = new File("InputFile.dat");
-        Scanner sc = new Scanner(file);
-        String output = "";
+        File file = new File("InputFile1.dat");
+        if (file.exists()) {
+            Scanner sc = new Scanner(file);
 
-        if (file.length() == 0) {
-            System.out.println("empty file");
-            output = "empty file";
-        }
+            if (file.length() == 0) {
+                System.out.println("empty file");
+                output = "empty file";
+            }
 
-        while (sc.hasNextLine()) {
-            String line = sc.nextLine();
-            CountUrinals cu = new CountUrinals(line);
-            int count = cu.countUrinals();
-            System.out.println("input: " + line + " , count: " + count);
-            output = output + count + "\n";
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                CountUrinals cu = new CountUrinals(line);
+                int count = cu.countUrinals();
+                System.out.println("input: " + line + " , count: " + count);
+                output = output + count + "\n";
+            }
+
+        }else{
+            System.out.println("file doesn't exist");
+            output = "";
         }
         WriteOutputToFile w = new WriteOutputToFile(output);
     }
